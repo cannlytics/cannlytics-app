7e604127abcd186488f868128aa9fc3bc35e6dcf
Keegan Skeate <keeganskeate@gmail.com>
2021-03-07 09:19:24 -0800

    preparing to use dart null-safety upgrade

--- lib/core_packages.dart
+++ lib/core_packages.dart
@@ -1,4 +1,4 @@
-// export 'package:darq/darq.dart'; // Extra list sorting methods sortByDescending, etc
+export 'package:darq/darq.dart'; // Extra list sorting methods sortByDescending, etc
 export 'package:cannlytics_app/styled_widgets/styled_widgets.dart';
 export 'package:provider/provider.dart'; // Context extensions for Provider, .watch() etc
 export 'package:sized_context/sized_context.dart'; // Shortcut for MediaQuery info, context.sizePx.with, etc
